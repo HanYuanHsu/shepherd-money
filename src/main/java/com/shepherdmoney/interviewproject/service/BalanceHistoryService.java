94c6dfe7150284f1f69afc52f3366d004d25bbeb
HanYuanHsu <a0306g@gmail.com>
2023-08-25 04:10:05 -0700

    fixing BalanceHistoryService

--- src/main/java/com/shepherdmoney/interviewproject/service/BalanceHistoryService.java
+++ src/main/java/com/shepherdmoney/interviewproject/service/BalanceHistoryService.java
@@ -31,22 +31,40 @@ public class BalanceHistoryService {
         Instant payloadTime = payload.getTransactionTime();
         double payloadAmount = payload.getTransactionAmount();
         List<BalanceHistory> balanceHistoryList = card.getBalanceHistory();
-
-        boolean shouldAddNewBalanceHistory = true;
+        boolean newBalanceHistoryIsEarliest = true;
         for (BalanceHistory currentBalanceHistory : balanceHistoryList) {
             Instant currentDate = currentBalanceHistory.getDate();
             int cmp = payloadTime.compareTo(currentDate);
             // cmp > 0 means that payload is more recent
             // cmp < 0 means that the current balance history is more recent
-            if (cmp <= 0) {
-                currentBalanceHistory.addMoney(payloadAmount);
-            }
+            if (cmp > 0) {
+                // add the new balance history created from the payload
+                BalanceHistory newBalanceHistory = new BalanceHistory(payloadTime,
+                        payloadAmount + currentBalanceHistory.getBalance());
+                balanceHistoryList.add(newBalanceHistory);
 
-            if (cmp == 0) shouldAddNewBalanceHistory = false;
+                // this means that the new balance history given by payload is not the earliest
+                newBalanceHistoryIsEarliest = false;
+
+                break;
+
+            } else if (cmp < 0) {
+                currentBalanceHistory.addMoney(payloadAmount);
+            } else {
+                currentBalanceHistory.addMoney(payloadAmount);
+
+                // this means that the new balance history given by payload is not the earliest
+                newBalanceHistoryIsEarliest = false;
+
+                break;
+            }
         }
 
-        if (shouldAddNewBalanceHistory) {
-            balanceHistoryList.add(new BalanceHistory(payloadTime, payloadAmount));
+        // edge case: when the new balance history given by payload is the earliest,
+        // we still need to add this new balance history into our balanceHistoryList.
+        if (newBalanceHistoryIsEarliest) {
+            BalanceHistory newBalanceHistory = new BalanceHistory(payloadTime, payloadAmount);
+            balanceHistoryList.add(newBalanceHistory);
         }
 
         // persistence
